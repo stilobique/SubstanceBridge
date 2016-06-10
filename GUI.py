f0f5a5b0f43f90be7533519ed7eac8d2971fb473
stilobique <availlant@novaquark.com>
2016-06-10 15:01:01 +0200

    Update GUI, add a file path for .spp file ; used to update an painter project.

--- GUI.py
+++ GUI.py
@@ -8,8 +8,18 @@ class PainterPanel(bpy.types.Panel):
     bl_space_type = "VIEW_3D"
     bl_region_type = "TOOLS"
     bl_category = "Tools"
+    
+    bpy.types.Scene.sppfile = bpy.props.StringProperty \
+        (
+        name = ".spp file",
+        description = "Field project path",
+        subtype = 'DIR_PATH'
+        )
  
     def draw(self, context):
         self.layout.label(text="Substance Painter")
         self.layout.operator("object.painter_export", text="New Project")
+        
+        self.layout.label(text="Substance Painter Project")
+        self.layout.prop(context.scene, 'sppfile')
         self.layout.operator("object.painter_export", text="Re-export")
