ad38d76e7a176ebb84e3032e61490e4b3472b957
aurelienvlt@free.fr <aurelienvlt@free.fr>
2016-06-18 00:19:05 +0200

    Finish Substance Painter Thread, now this addon are fonctional with a basic stat ; we can create a new substance painter project or edit an existing file.

--- CommandPainter.py
+++ CommandPainter.py
@@ -3,41 +3,28 @@ import threading
 import subprocess
 import os
 
-from threading import Thread
-
 from bpy.props import StringProperty, BoolProperty
 from SubstanceBridge.settings import SubstanceSettings
 
 # ------------------------------------------------------------------------
 # Create a class for a generic thread, else blender are block.
 # ------------------------------------------------------------------------
-class PainterThread(threading.Thread):
-
-    project = StringProperty(
-            name="Substance Painter",
-            default = '',
-            subtype='FILE_PATH',
-            )
-            
-    def run(self):        
+class SubstancePainterThread(threading.Thread):
+    def __init__(self, path_painter, path_project):
+        threading.Thread.__init__(self)
+        self.path_painter = path_painter
+        self.path_project = path_project
+        
+    def run(self):
         temp_folder = bpy.app.tempdir
-        temp_mesh = 'zob.obj'
+        temp_mesh = 'tmp.obj'
         mesh = temp_folder + temp_mesh
-        # project = None
-        project = path_sppfile = os.path.abspath(bpy.context.scene.sppfile)
-        
-        user_preferences = bpy.context.user_preferences
-        addon_prefs = user_preferences.addons["SubstanceBridge"].preferences
-        painter = str(addon_prefs.painterpath) # Set path for instant meshes
-        
-        if project == None:
-            popen = subprocess.call([painter, '--mesh', mesh])
-            print("News painter project") # Debug lines
+        if self.path_project == "":
+            popen = subprocess.call([self.path_painter, '--mesh', mesh])
             
         else:
-            popen = subprocess.call([painter, '--mesh', mesh, project])
-            print("Update painter project") # Debug lines
-            return {'CANCELLED'}
+            popen = subprocess.call([self.path_painter, '--mesh', mesh, self.path_project])
+
 # ------------------------------------------------------------------------
 # Function to create an Obj, and export to painter
 # ------------------------------------------------------------------------
@@ -59,58 +46,49 @@ class PainterProject(bpy.types.Operator):
         name = "Variable de test, update or not"
         )
         
+    path_project = StringProperty(
+        name = "Path Substance project"
+        )
+
     def execute(self, context):
         # All variable for the script
         obj = bpy.context.active_object
      
         temp_folder = bpy.app.tempdir
-        temp_mesh = 'zob.obj'
+        temp_mesh = 'tmp.obj'
         mesh = temp_folder + temp_mesh
         
-        # Test pour un nouveau project ou l'update d'un project.
-        if self.project == True:
-            user_preferences = bpy.context.user_preferences
-            addon_prefs = user_preferences.addons["SubstanceBridge"].preferences
-            self.painter = str(addon_prefs.painterpath) # Set path for instant meshes
-
-            path_sppfile = os.path.abspath(bpy.context.scene.sppfile)
-            print("path du spp file relatif : ", path_sppfile) # Debug lines
-            print("Project variable : ", self.project) # Debug lines
-            print("painter : ", self.painter) # Debug lines
+        user_preferences = bpy.context.user_preferences
+        addon_prefs = user_preferences.addons["SubstanceBridge"].preferences
+        self.painter = str(addon_prefs.painterpath) # Set path for instant meshes
         
-        # Il s'agit d'un nouveau project.
-        else:
-            if obj.type == 'MESH':
-                if bpy.data.meshes[obj.name].uv_textures:
-                    # Export du mesh selectionne
-                    bpy.ops.export_scene.obj(filepath=mesh, use_selection=True, path_mode='AUTO')
-                    user_preferences = bpy.context.user_preferences
-                    addon_prefs = user_preferences.addons["SubstanceBridge"].preferences
-                    self.painter = str(addon_prefs.painterpath) # Set path for instant meshes
-                    # Verification si le soft est configuré dans le path
-                    if self.painter:
-                        self.update = False
+        if obj.type == 'MESH':
+            if bpy.data.meshes[obj.name].uv_textures:
+                # Export du mesh selectionne
+                bpy.ops.export_scene.obj(filepath=mesh, use_selection=True, path_mode='AUTO')
+                
+                # Verification si le soft est configuré dans le path
+                if self.painter:
+                    # Test If it's a new project.
+                    if self.project == True:
                         path_sppfile = os.path.abspath(bpy.context.scene.sppfile)
-                        project = path_sppfile
-                    
-                        """Launch substance painter program."""
-                        launchpainter = PainterThread(args=(project,))
-                        # launchpainter = PainterThread()
+                        self.path_project = path_sppfile
+                        launchpainter = SubstancePainterThread(self.painter, self.path_project)
                         launchpainter.start()
-                        
-                        print("Update ? >> ", self.update) # Debug lines
-
                     else:
-                        print("path de substance painter %s : ", self.painter) # Debug lines
-                        self.report({'WARNING'}, "No path configured, setup into User Pre.")
-                        return {'CANCELLED'}
-
+                        self.path_project = ""
+                        launchpainter = SubstancePainterThread(self.painter, self.path_project)
+                        launchpainter.start()
                 else:
-                    self.report({'WARNING'}, "This object don't containt a UV layers.")
+                    self.report({'WARNING'}, "No path configured, setup into User Pre.")
                     return {'CANCELLED'}
 
             else:
-                self.report({'WARNING'}, "This object is not a mesh.")
+                self.report({'WARNING'}, "This object don't containt a UV layers.")
                 return {'CANCELLED'}
+
+        else:
+            self.report({'WARNING'}, "This object is not a mesh.")
+            return {'CANCELLED'}
 
         return {'FINISHED'}
