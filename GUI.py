c3cb2944f3435b977a4be23b2c73b2b7db8b0b9b
Vaillant Jeremy <vaillant.jeremy@dev-crea.com>
2016-11-22 00:02:38 +0100

    Add gourp button for texturing list

--- GUI.py
+++ GUI.py
@@ -1,45 +1,27 @@
 import bpy
-from bpy.props import StringProperty
 
 # from SubstanceBridge import list_high
 
 # ------------------------------------------------------------------------
 # Substance Painter panel.
 # ------------------------------------------------------------------------
-class PainterPanel(bpy.types.Panel): 
+class PainterPanel(bpy.types.Panel):
     bl_label = "Substance Painter"
     bl_space_type = "VIEW_3D"
     bl_region_type = "TOOLS"
     bl_category = "Substances"
-            
+
     def draw(self, context):
-        self.layout.operator("object.painter_export", text="New Project").project = False
-        project_file = self.layout.prop(context.scene, 'sppfile')
-        self.layout.operator("object.painter_export", text="Re-export").project = True
-        
+        layout = self.layout
 
-        
-# # ------------------------------------------------------------------------
-# # Substance BatchTools panel.
-# # ------------------------------------------------------------------------
-# class BatchToolsPanel(bpy.types.Panel): 
-    # bl_label = "Substance BatchTools"
-    # bl_space_type = "VIEW_3D"
-    # bl_region_type = "TOOLS"
-    # bl_category = "Substances"
-    
-    # def draw(self, context):
-        # layout = self.layout
-        
-        # layout.label(text="Baking")
-        # # layout.prop_search(context.scene, "lp_name", context.scene, "lp_object", icon='OBJECT_DATA')
-        
-        # layout.prop_search(data, property, search_data, search_property, text, text_ctxt, translate, icon='OBJECT_DATA')
+        name = "New Project"
+        layout.operator("object.painter_export", name).project = False
 
-        # ob = context.object
+        type_file = 'sppfile'
+        layout.prop(context.scene, type_file)
 
-        # split = layout.split()
+        name = 'Update'
+        layout.operator("object.painter_export", name).project = True
 
-        # col = split.column()
-        # col.prop(ob, "parent", text="")
- 
+        name = 'Texturing List'
+        layout.operator("group.create", name)
