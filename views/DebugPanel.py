f2b9d15a74ffd8c5a14e9c9ae95c9b80f03557dd
aurelienvlt@free.fr <aurelienvlt@free.fr>
2017-01-03 22:21:09 +0100

    Create an simple operator debug and simple button to call it.

--- views/DebugPanel.py
+++ views/DebugPanel.py
@@ -2,6 +2,25 @@ import bpy
 
 
 # -----------------------------------------------------------------------------
+# Function Debug, show many proporties
+# -----------------------------------------------------------------------------
+class DebugShow(bpy.types.Operator):
+    """Tooltip"""
+    bl_idname = "debug.sbs_data"
+    bl_label = "A function to show many substance data debug"
+
+    def execute(self, context):
+        scn = context.scene
+
+        # Substance Variable
+        sbs_name = scn.sbs_project_settings.prj_name
+
+        print(scn)
+
+        return {'FINISHED'}
+
+
+# -----------------------------------------------------------------------------
 # Substance Project panel
 # -----------------------------------------------------------------------------
 class DebugPanel(bpy.types.Panel):
@@ -14,28 +33,16 @@ class DebugPanel(bpy.types.Panel):
         layout = self.layout
         scn = context.scene
 
-        row = layout.row(align=True)
-        icon = "GROUP"
-        row.prop(scn, 'project_name', text='Painter Project', icon=icon)
-        icon = "ZOOMIN"
-        row.operator("painter.substance_name", text="", icon=icon)
-        icon = "RESTRICT_SELECT_OFF"
-        row.operator("painter.selected_project", text="", icon=icon)
+        layout.label("Debug Call")
 
-        layout.label("Project Name in this file")
-
-        all_obj = bpy.data.objects
-
-        for obj in all_obj:
-            if obj.get('substance_project') is not None:
-                name_obj = bpy.data.objects[obj.name]
-                name_prj = bpy.data.objects[obj.name]['substance_project']
-                layout.label(name_prj)
+        layout.operator("debug.sbs_data", text="Debug Sbs")
 
 
 def register():
+    bpy.utils.register_class(DebugShow)
     bpy.utils.register_class(DebugPanel)
 
 
 def unregister():
+    bpy.utils.unregister_class(DebugShow)
     bpy.utils.unregister_class(DebugPanel)
