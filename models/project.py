a53fc22d1f9b383d502cd1f2eedf33122bfcfa7d
aurelienvlt@free.fr <aurelienvlt@free.fr>
2017-01-03 22:20:13 +0100

    Change data, use an pointerproperty to save sbs data.

--- models/project.py
+++ models/project.py
@@ -1,14 +1,3 @@
-# Project Substance
-#  -> Name -> String
-#  -> Path .sppfile
-#  -> Mesh(s)
-#       -> furnitur (example)
-#       -> drawers right (example)
-#       -> drawers left (example)
-#   -> Texture Set List
-#       -> furnitur (material)
-#       -> drawers (material)
-
 import bpy
 
 from bpy.props import *
@@ -19,10 +8,10 @@ from bpy.props import *
 # ----------------------------------------------------------------------------
 class SubstanceProjectItems(bpy.types.PropertyGroup):
     # Name of Substance Project
-    # project_name = StringProperty(
-    #     name="Project Name",
-    #     default="Default",
-    #     )
+    prj_name = bpy.props.StringProperty(
+        name="Project Name",
+        default="Sbs Project",
+        )
     # Path File to edit this project
     path_spp = StringProperty(
         name="Project Path",
@@ -31,10 +20,8 @@ class SubstanceProjectItems(bpy.types.PropertyGroup):
         subtype='FILE_PATH'
     )
     # List of mesh
-    meshs_index = StringProperty(
-        name="Name Set",
-        default="Default",
-        )
+    meshs_name = []
+
     # List of Texture Set
     tx_set_index = StringProperty(
         name="Name Set",
@@ -59,13 +46,12 @@ class TextureSetListItems(bpy.types.PropertyGroup):
         default="Default",
         )
 
+
 def register():
     bpy.utils.register_class(SubstanceProjectItems)
-    bpy.types.Scene.project_settings = \
-        bpy.props.CollectionProperty(type=SubstanceProjectItems)
+    bpy.types.Scene.sbs_project_settings = \
+        bpy.props.PointerProperty(type=SubstanceProjectItems)
     # Nominate project variable
-    bpy.types.Scene.project_name = \
-        StringProperty(default="substance project")
     bpy.types.Scene.sppfile = StringProperty(
         name="Project Path",
         default="",
@@ -80,8 +66,7 @@ def register():
 
 def unregister():
     # Nominate project variable
-    del bpy.types.Scene.project_settings
-    del bpy.types.Scene.project_name
+    del bpy.types.Scene.sbs_project_settings
     del bpy.types.Scene.sppfile
     # Nominate texture set variable
     del bpy.types.Scene.tx_set_list
