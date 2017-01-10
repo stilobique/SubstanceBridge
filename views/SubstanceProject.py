3870b845e5460a89130d48a0fa2e23c3e5c94cd7
Aurelien <aurelienvlt@free.fr>
2017-01-10 22:43:27 +0100

    Now, the sbs name field change if the selected object have this data.

--- views/SubstanceProject.py
+++ views/SubstanceProject.py
@@ -14,11 +14,20 @@ class SubstanceProjectPanel(bpy.types.Panel):
         layout = self.layout
         scn = context.scene
         obj = context.object
+        act = context.active_object
 
         layout.label("Project Name")
         row = layout.row(align=True)
+        # Check if this object as an Sbs Project.
         data = scn.sbs_project_settings
-        row.prop(data, 'prj_name', text="")
+        if act.get('substance_project') is not None:
+            sbs_obj = bpy.context.active_object['substance_project']
+            scene_name = bpy.context.scene.name
+            scene = bpy.data.scenes[scene_name]['sbs_project_settings']
+            scene['prj_name'] = sbs_obj
+
+        row.prop(data, 'prj_name', text="new")
+
         icon = "ZOOMIN"
         row.operator("painter.substance_name", text="", icon=icon)
         icon = "RESTRICT_SELECT_OFF"
