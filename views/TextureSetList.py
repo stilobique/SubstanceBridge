38d088dd19375bcc6e04880217b101863f2532b8
aurelienvlt@free.fr <aurelienvlt@free.fr>
2017-02-21 23:09:37 +0100

    Update color texture set. Best UI setup.

--- views/TextureSetList.py
+++ views/TextureSetList.py
@@ -32,6 +32,9 @@ class TextureSetListPanel(bpy.types.Panel):
                 ops = "sbs_painter.uv_set_on"
                 row.operator(ops, text="", icon=icon).index = index
 
+            prop = "diffuse_color"
+            row.prop(shader[mat.name], prop, icon=icon, icon_only=True)
+
         name = "Add a Set"
         layout.operator("sbs_painter.uv_set_add", name)
 
