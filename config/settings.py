9a55fc03727a05115a42232f5e572015a3086f00
stilobique <stilobique@users.noreply.github.com>
2017-03-26 21:29:03 +0200

    Refactore settings files, more best MVC use

--- config/settings.py
+++ config/settings.py
@@ -1,76 +1,36 @@
 import bpy
 
-
-from bpy.props import BoolProperty, IntProperty, StringProperty
 from .. import addon_updater_ops
+
+# Import all variable setup
+from ..models.paths import SbsModelsSetup
+from ..models.addon_updater_setup import UpdaterVariables
+
 
 # -----------------------------------------------------------------------------
 # Settings for this addons
 # -----------------------------------------------------------------------------
-
-
 class SubstanceSettings(bpy.types.AddonPreferences):
     bl_idname = "SubstanceBridge"
 
     # addon updater preferences from `__init__`, be sure to copy all of them
-    auto_check_update = BoolProperty(
-        name="Auto-check for Update",
-        description="If enabled, auto-check for updates using an interval",
-        default=False,
-    )
+    auto_check_update = UpdaterVariables.auto_check_update
 
-    updater_intrval_months = IntProperty(
-        name='Months',
-        description="Number of months between checking for updates",
-        default=0,
-        min=0
-    )
-    updater_intrval_days = IntProperty(
-        name='Days',
-        description="Number of days between checking for updates",
-        default=7,
-        min=0,
-    )
-    updater_intrval_hours = IntProperty(
-        name='Hours',
-        description="Number of hours between checking for updates",
-        default=0,
-        min=0,
-        max=23
-    )
-    updater_intrval_minutes = IntProperty(
-        name='Minutes',
-        description="Number of minutes between checking for updates",
-        default=0,
-        min=0,
-        max=59
-    )
+    updater_intrval_months = UpdaterVariables.updater_intrval_months
+    updater_intrval_days = UpdaterVariables.updater_intrval_days
+    updater_intrval_hours = UpdaterVariables.updater_intrval_hours
+    updater_intrval_minutes = UpdaterVariables.updater_intrval_minutes
 
     # All software path.
-    path_painter = StringProperty(
-            name="Substance Painter",
-            subtype="FILE_PATH",
-            )
-    path_designer = StringProperty(
-            name="Substance Designer",
-            subtype='FILE_PATH',
-            )
-    path_batchtools = StringProperty(
-            name="Batch Tools",
-            subtype='DIR_PATH',
-            )
-    path_shelf_sbs = StringProperty(
-            name="Shelf SBS",
-            subtype='DIR_PATH',
-            )
+    path_painter = SbsModelsSetup.path_painter
 
     def draw(self, context):
         layout = self.layout
         layout.label(text="Substance Path.")
 
-        row = layout.row(align=True)
+        row = layout.row(align=False)
         row.prop(self, "path_painter")
-        row.operator("substance.check", text="Check")
+        row.operator("substance.check", text="Check").path = self.path_painter
         # layout.prop(self, "path_batchtools")
         # layout.prop(self, "path_designer")
 
