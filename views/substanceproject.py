32eebfd0714df8ce85af9d0960677718d45c3938
aurelienvlt@free.fr <aurelienvlt@free.fr>
2017-04-03 22:23:36 +0200

    Add more condition to all panel, corrected many error when no object are selected

--- views/substanceproject.py
+++ views/substanceproject.py
@@ -24,42 +24,43 @@ class SubstanceProjectPanel(bpy.types.Panel):
 
         row = layout.row(align=True)
 
-        # Check if this object as an Sbs Project.
-        if act.get('substance_project') is not None:
-            sbs_obj = bpy.context.active_object['substance_project']
-            scene_name = bpy.context.scene.name
-            scene = bpy.data.scenes[scene_name]['sbs_project_settings']
-            scene['prj_name'] = sbs_obj
+        if act:
+            # Check if this object as an Sbs Project.
+            if act.get('substance_project') is not None:
+                sbs_obj = bpy.context.active_object['substance_project']
+                scene_name = bpy.context.scene.name
+                scene = bpy.data.scenes[scene_name]['sbs_project_settings']
+                scene['prj_name'] = sbs_obj
 
-        row.prop(data, 'prj_name', text="")
+            row.prop(data, 'prj_name', text="")
 
-        # Panel when the selected object has no Substance Project
-        if obj.get("substance_project") is None:
-            icon = "ZOOMIN"
-            row.operator("sbs_painter.substance_name", text="", icon=icon)
+            # Panel when the selected object has no Substance Project
+            if obj.get("substance_project") is None:
+                icon = "ZOOMIN"
+                row.operator("sbs_painter.substance_name", text="", icon=icon)
 
-            # Not Substance Project in this blend-file
-            layout.label("Create a New Project")
+                # Not Substance Project in this blend-file
+                layout.label("Create a New Project")
 
-        # If the mesh use a Substance Project
-        else:
-            icon = "ZOOMIN"
-            row.operator("sbs_painter.substance_name", text="", icon=icon)
-            icon = "RESTRICT_SELECT_OFF"
-            row.operator("sbs_painter.selected_project", text="", icon=icon)
-            icon = "PANEL_CLOSE"
-            row.operator("sbs_painter.remove_from_project", text="", icon=icon)
+            # If the mesh use a Substance Project
+            else:
+                icon = "ZOOMIN"
+                row.operator("sbs_painter.substance_name", text="", icon=icon)
+                icon = "RESTRICT_SELECT_OFF"
+                row.operator("sbs_painter.selected_project", text="", icon=icon)
+                icon = "PANEL_CLOSE"
+                row.operator("sbs_painter.remove_from_project", text="", icon=icon)
 
-            name = "Export New Project"
-            ops = "substance.painter_export"
-            layout.operator(ops, name).project = False
+                name = "Export New Project"
+                ops = "substance.painter_export"
+                layout.operator(ops, name).project = False
 
-            data = scn.sbs_project_settings
-            layout.prop(data, 'path_spp', text="")
+                data = scn.sbs_project_settings
+                layout.prop(data, 'path_spp', text="")
 
-            name = 'Export Update'
-            icon = 'FILE_REFRESH'
-            layout.operator(ops, name, icon=icon).project = True
+                name = 'Export Update'
+                icon = 'FILE_REFRESH'
+                layout.operator(ops, name, icon=icon).project = True
 
 
 def register():
