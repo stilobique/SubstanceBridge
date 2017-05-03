753af3863eb10a739ea7d2c70bffd19e22d0cfb2
aurelienvlt@free.fr <aurelienvlt@free.fr>
2017-05-03 16:44:16 +0200

    Add a new rule, if the selected object isn't a Substance Project, don't show the texture set list

--- views/texturesetlist.py
+++ views/texturesetlist.py
@@ -4,6 +4,9 @@ import bpy
 # -----------------------------------------------------------------------------
 # Texture Set List panel
 # -----------------------------------------------------------------------------
+from operator import concat
+
+
 class TextureSetListPanel(bpy.types.Panel):
     bl_label = "Texture Set List"
     bl_space_type = "VIEW_3D"
@@ -16,29 +19,31 @@ class TextureSetListPanel(bpy.types.Panel):
         obj = context.object
         act_obj = context.active_object
 
-        if act_obj:
-            for mat in obj.material_slots:
-                shader = bpy.data.materials
-                row = layout.row(align=True)
 
-                icon = "MATERIAL"
-                row.prop(shader[mat.name], "name", text="", icon=icon)
-                icon = "GROUP_UVS"
-                row.operator("sbs_painter.uv_set", text="", icon=icon)
+        if act_obj.get('substance_project') is not None:
+            if act_obj:
+                for mat in obj.material_slots:
+                    shader = bpy.data.materials
+                    row = layout.row(align=True)
 
-                liste = act_obj.material_slots
-                index = liste.find(mat.name)
+                    icon = "MATERIAL"
+                    row.prop(shader[mat.name], "name", text="", icon=icon)
+                    icon = "GROUP_UVS"
+                    row.operator("sbs_painter.uv_set", text="", icon=icon)
 
-                if index > 0:
-                    icon = "SPACE3"
-                    ops = "sbs_painter.uv_set_on"
-                    row.operator(ops, text="", icon=icon).index = index
+                    liste = act_obj.material_slots
+                    index = liste.find(mat.name)
 
-                prop = "diffuse_color"
-                row.prop(shader[mat.name], prop, icon=icon, icon_only=True)
+                    if index > 0:
+                        icon = "SPACE3"
+                        ops = "sbs_painter.uv_set_on"
+                        row.operator(ops, text="", icon=icon).index = index
 
-            name = "Add a Set"
-            layout.operator("sbs_painter.uv_set_add", name)
+                    prop = "diffuse_color"
+                    row.prop(shader[mat.name], prop, icon=icon, icon_only=True)
+
+                name = "Add a Set"
+                layout.operator("sbs_painter.uv_set_add", name)
 
 
 def register():
