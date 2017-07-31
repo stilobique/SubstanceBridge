5ecb6ea64d51265aaa8c59f45dd27448cf23e02a
aurelienvlt@free.fr <aurelienvlt@free.fr>
2017-07-31 23:54:48 +0200

    Edit documentation, needed to be more clear :) .

--- models/SubstanceSoftware.py
+++ models/SubstanceSoftware.py
@@ -8,16 +8,28 @@ class to call a software, this number version, this path or many option.
 """
 
 
-class SubstancePainter(threading.Thread):
+class SubstancePainterLaunch(threading.Thread):
     """Substance Painter Thread, any argument to launch an new instance
-    project, check the Painter Version and other action."""
+    project, check the Painter Version and other action.
+
+    You can find all Substance Painter Command Lines on Allegorithmic
+    documentation.
+
+    https://docs.allegorithmic.com/documentation/display/SPDOC/Command+Lines"""
     def __init__(self, path_painter, path_project, name_project):
         threading.Thread.__init__(self)
         self.path_painter = path_painter
         self.path_project = path_project
         self.name_project = name_project
 
-    def launch(self):
+    def run(self):
+        # All Command
+        # --help
+        # -version
+        # --mesh <meshPath>
+        # --mesh-map
+        # -export-path
+
         if self.path_project == "":
             subprocess.call([self.path_painter,
                              '--mesh',
@@ -30,9 +42,3 @@ class SubstancePainter(threading.Thread):
                              self.name_project,
                              self.path_project,
                              ])
-
-    def version(self):
-        subprocess.call([self.path_painter,
-                         '--mesh',
-                         self.name_project,
-                         ])
