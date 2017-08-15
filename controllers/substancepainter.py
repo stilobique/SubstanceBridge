df703451029641712fa01f0be166e1db76351f28
aurelienvlt@free.fr <aurelienvlt@free.fr>
2017-08-15 11:24:53 +0200

    Remove unused import

--- controllers/substancepainter.py
+++ controllers/substancepainter.py
@@ -8,8 +8,6 @@
 import bpy
 
 from ..models.SubstanceSoftware import *
-from bpy.props import StringProperty, BoolProperty
-
 
 # ------------------------------------------------------------------------
 # Function to define the obj name.
