23e701257af1bf9567e839ab664d6d1e2def8ba4
aurelienvlt@free.fr <aurelienvlt@free.fr>
2017-08-15 11:46:00 +0200

    Clean code, remove unused line and simplified it.

--- controllers/substancecontroller.py
+++ controllers/substancecontroller.py
@@ -11,7 +11,6 @@
 # -----------------------------------------------------------------------------
 
 import bpy
-from bpy.props import IntProperty
 
 
 # -----------------------------------------------------------------------------
@@ -23,6 +22,11 @@ class CreateSubstanceProject(bpy.types.Operator):
     bl_idname = "sbs_painter.substance_name"
     bl_label = "Create a new Substance Painter project"
 
+    @classmethod
+    def poll(cls, context):
+        ob = context.active_object
+        return ob is not None and ob.type == 'MESH'
+
     def execute(self, context):
         scn = context.scene
         slct_obj = context.selected_objects
@@ -30,30 +34,30 @@ class CreateSubstanceProject(bpy.types.Operator):
 
         # Check if this object can be export for Sbs Painter.
         for obj in slct_obj:
-            if obj.type == 'MESH':
-                # Add attribute for all mesh selected
-                obj['substance_project'] = sbs_settings.prj_name
+            # if obj.type == 'MESH':
+            # Add attribute for all mesh selected
+            obj['substance_project'] = sbs_settings.prj_name
 
-                # Clear Material slot
-                nbr = len(bpy.data.objects[obj.name].material_slots)
-                for i in range(nbr):
-                    bpy.ops.object.material_slot_remove()
+            # Clear Material slot
+            nbr = len(bpy.data.objects[obj.name].material_slots)
+            for i in range(nbr):
+                bpy.ops.object.material_slot_remove()
 
-                # Add a materials basic.
-                base_mat = bpy.data.materials.get(sbs_settings.prj_name)
-                if base_mat is None:
-                    base_mat = bpy.data.materials.new(sbs_settings.prj_name)
+            # Add a materials basic.
+            base_mat = bpy.data.materials.get(sbs_settings.prj_name)
+            if base_mat is None:
+                base_mat = bpy.data.materials.new(sbs_settings.prj_name)
 
-                # Asign materials to all object selected
-                if obj.data.materials:
-                    obj.data.materials[0] = base_mat
-
-                else:
-                    obj.data.materials.append(base_mat)
+            # Asign materials to all object selected
+            if obj.data.materials:
+                obj.data.materials[0] = base_mat
 
             else:
-                self.report({'WARNING'}, "This object is not a mesh.")
-                return {'CANCELLED'}
+                obj.data.materials.append(base_mat)
+
+            # else:
+            #     self.report({'WARNING'}, "This object is not a mesh.")
+            #     return {'CANCELLED'}
 
         return {'FINISHED'}
 
@@ -134,7 +138,6 @@ class TextureSetAdd(bpy.types.Operator):
         all_obj = bpy.data.objects
         slc_obj = context.active_object
         project_name = scn.sbs_project_settings.prj_name
-        sbs_settings = scn.sbs_project_settings
 
         # Create a new material.
         nbr = len(slc_obj.material_slots)
@@ -143,7 +146,6 @@ class TextureSetAdd(bpy.types.Operator):
         new_mat = bpy.data.materials.new(name_mat)
 
         for obj in all_obj:
-            name_obj = bpy.data.objects[obj.name]
             if obj.get('substance_project') is not None:
                 name_prj = bpy.data.objects[obj.name]['substance_project']
                 if name_prj == project_name:
@@ -161,7 +163,7 @@ class TextureSetOn(bpy.types.Operator):
     bl_label = "Make an set list on first."
 
     # Generate a index variable, need to count all set list used.
-    index = IntProperty(
+    index = bpy.props.IntProperty(
         name="Index Material",
         default=0,
     )
